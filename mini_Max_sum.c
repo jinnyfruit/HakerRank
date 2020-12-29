e77ca9d3ad5abf904002cd92771b7e5ee407d230
jinnyfruit <jinnyfruit0527@gmail.com>
2020-12-29 21:11:37 +0900

    final code

--- mini_Max_sum.c
+++ mini_Max_sum.c
@@ -14,24 +14,19 @@ char** split_string(char*);
 // Complete the miniMaxSum function below.
 void miniMaxSum(int arr_count, int* arr) {  //배열에서 가장 작은 합과 가장 큰 합을 출력하시오.
    unsigned int sum=0;
-    int min,max;
     int* cpy=arr;
-
-    for(int i=0;i<arr_count;i++){   //get the total sum of array
-        sum+=*cpy;
-        cpy++;
-    }
-    cpy=arr;    //initialize the cpy value
+    int min=*cpy,max=*cpy;
     int value;
-    min=*cpy,max=*cpy;
-    for(int i=0;i<arr_count;i++){
+    for(int i=0;i<arr_count;i++){   //get the total sum of array
         value=*cpy;
+        sum+=*cpy;
         if(value<=min)
             min=value;
         if(max<=value)
             max=value;
         cpy++;
     }
+   
     printf("%u %u",sum-max,sum-min);
 }
 int main()
